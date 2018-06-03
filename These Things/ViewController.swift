7926f371b767f406c716f6fe52d30ba5752ba911
iSw4gger <jaredboynton04@gmail.com>
2018-06-03 00:40:11 -0500

    Initial Commit

---
+++ These Things/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  These Things
+//
+//  Created by Jared Boynton on 6/3/18.
+//  Copyright © 2018 Jared Boynton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
